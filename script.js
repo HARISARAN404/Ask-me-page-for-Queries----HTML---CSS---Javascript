a35c17829ee05b9f8c8219b6d7e9fa9592fd03ff
HARISARAN405 <113167317+HARISARAN405@users.noreply.github.com>
2024-07-23 16:19:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.accept').addEventListener('click', () => {
+    alert('Accepted');
+});
+
+document.querySelector('.decline').addEventListener('click', () => {
+    alert('Declined');
+});
